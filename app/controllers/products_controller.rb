a88997ec7d780d25abdebc9d2c10dea982d4333b
“Joseph <joseph@josephs-air.lan>
2021-10-14 19:36:07 -0700

    updated routs for sget show all/ show/ create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,22 @@
 class ProductsController < ApplicationController
-  def showall
+  def index
     products = Product.all
     render json: products.as_json
+  end
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      imagine_url: params[:input_imagine_url],
+      description: params[:description],
+    )
+    product.save
+    render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "/products" => "products#index"
 
-get "/showall", controller: "products", action: "showall"
+  get "/show/:id" => "products#show"
 
+  post "/products" => "products#create"
 end
